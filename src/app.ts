143e8caeab982eb7cd4bb667f2b81ad25a1a9f40
jessur2030 <jessusdev@gmail.com>
2023-10-09 15:17:34 -0400

    add register userRautes

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import Fastify from 'fastify';
+import userRautes from './user/user.route';
 const server = Fastify();
 const PORT = process.env.PORT || 9000;
 server.get('/healthcheck', async function (request, reply) {
 return {status: 'ok'}
 });
 async function main(){
+
+    server.register(userRautes, {prefix: '/api/users'});
     try {
         await server.listen(PORT, '0.0.0.0')
         console.log(`Server running at localhost:${PORT}`)
