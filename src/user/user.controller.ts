51f8f013890dfbc91114fcaa87f46f4a060b09ae
jessur2030 <jessusdev@gmail.com>
2023-10-27 16:29:39 -0400

    add registerUserHandler controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,5 +1,16 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { createUserSchemaType } from "./user.schema";
+import { createrUser } from "./user.service";
 
-export default async function registerUserHandler(request: FastifyRequest, reply: FastifyReply){
-    return 'register user function called';
+export default async function registerUserHandler(request: FastifyRequest<{Body: createUserSchemaType}>, reply: FastifyReply){
+
+    const body = request.body;
+    console.log(body)
+   try {
+    const user = await createrUser(body);
+    return reply.code(201).send(user)
+   } catch (error) {
+    console.error(error)
+    return reply.code(500).send(error)
+   }
 }
