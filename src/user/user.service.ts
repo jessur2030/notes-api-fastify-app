9079adb7c2c8b3f85d57df283a9abe1924c951ba
jessur2030 <jessusdev@gmail.com>
2023-10-27 15:52:33 -0400

    add fixes to createrUser service func

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,6 +4,8 @@ import { createUserSchemaType } from "./user.schema";
 
 export async function createrUser (input: createUserSchemaType){
     const {password, ...rest} = input;
-    const {salt, hash} = hashPassword(password);
-    const user = await prisma.users.create({data: {...rest, password: hash, salt}})
+    const {salt, hashPassword: hashedPassword } = hashPassword(password);
+    const user = await prisma.users.create({data: {...rest, password: hashedPassword, salt}})
+    console.log(user)
+    return user;
 } 
