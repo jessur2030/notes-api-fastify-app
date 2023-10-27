3ec77ba27514127cd33c9d6c1efb83f47abed478
jessur2030 <jessusdev@gmail.com>
2023-10-27 15:45:56 -0400

    update hashPassword & verifyPassword

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -2,11 +2,11 @@ import crypto from 'crypto';
 
 export function hashPassword(password: string) {
     const salt = crypto.randomBytes(16).toString('hex');
-    const hash = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
-    return { salt, hash };
+    const hashPassword = crypto.pbkdf2Sync(password, salt, 1000, 64, 'sha512').toString('hex');
+    return { salt, hashPassword };
 }
 
-export function verifyPassword({candidatePassword, salt, hash}: {candidatePassword: string, salt: string, hash: string} ){
+export function verifyPassword({candidatePassword, salt, hashPassword}: {candidatePassword: string, salt: string, hashPassword: string} ){
     const candidateHash = crypto.pbkdf2Sync(candidatePassword, salt, 1000, 64, 'sha512').toString('hex');
-    return candidateHash === hash;
+    return candidateHash === hashPassword;
 }
