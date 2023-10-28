a9bab729ac0a0635f29c204a1afc71951d629e8d
jessur2030 <jessusdev@gmail.com>
2023-10-27 23:08:12 -0400

    add register userSchemas

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import Fastify from 'fastify';
-import userRautes from './user/user.route';
+import userRautes from './modules/user/user.route';
+import {userSchemas} from "./modules/user/user.schema";
 const server = Fastify();
 const PORT = process.env.PORT || 9000;
+
 server.get('/healthcheck', async function (request, reply) {
 return {status: 'ok'}
 });
+
 async function main(){
+
+    for (const schema of userSchemas) {
+        server.addSchema(schema)
+    }
 
     server.register(userRautes, {prefix: '/api/users'});
     try {
