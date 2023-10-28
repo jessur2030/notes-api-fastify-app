a731ed77816d5c5d05ccebb774e69deb5e6ff906
jessur2030 <jessusdev@gmail.com>
2023-10-27 23:08:35 -0400

    update path

--- src/user/user.route.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify";
-import registerUserHandler from "./user.controller";
-
- export default async function userRautes(server: FastifyInstance) {
-    return server.post('/', registerUserHandler);
- }
