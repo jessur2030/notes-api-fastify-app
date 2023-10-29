fbee153148d56bae9af4389c933e4aa443652c5c
jessur2030 <jessusdev@gmail.com>
2023-10-29 16:34:44 -0400

    add register fastifyEnv & fastifyJwt

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,38 @@
 import Fastify from 'fastify';
+import fastifyJwt from '@fastify/jwt';
+const fastifyEnv = require('@fastify/env')
 import userRautes from './modules/user/user.route';
 import {userSchemas} from "./modules/user/user.schema";
 const server = Fastify();
+
+const schema = {
+    type: 'object',
+    required: ['PORT', 'JWT_SECRET'],
+    properties: {
+        PORT: {
+            type: 'string',
+            default: 9000
+        },
+        JWT_SECRET: {
+            type: 'string',
+            default: 'supersecret'
+        }
+    }
+  }
+
+  const options = {
+    confKey: 'config',
+    schema,
+    dotenv: true,
+    data: process.env
+  }
+
+server.register(fastifyEnv, options)
+// register fastify/jwt
+server.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET as string
+})
+
 const PORT = process.env.PORT || 9000;
 
 server.get('/healthcheck', async function (request, reply) {
