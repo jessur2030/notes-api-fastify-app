fdcc1f12dd4ff3569733c2ea4653e711b9b9f054
jessur2030 <jessusdev@gmail.com>
2023-10-29 22:19:16 -0400

    add decorator authenticate

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,19 @@
-import Fastify from 'fastify';
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify';
 import fastifyJwt from '@fastify/jwt';
-import fastifyEnv  from '@fastify/env'
 import userRautes from './modules/user/user.route';
 import {userSchemas} from "./modules/user/user.schema";
 const server = Fastify();
 
-const schema = {
-    type: 'object',
-    required: ['PORT', 'JWT_SECRET'],
-    properties: {
-        PORT: {
-            type: 'string',
-            default: 9000
-        },
-        JWT_SECRET: {
-            type: 'string',
-            default: 'supersecret'
-        }
-    }
-  }
-
-  const options = {
-    confKey: 'config',
-    schema,
-    dotenv: true,
-    data: process.env
-  }
-
-server.register(fastifyEnv, options)
-// register fastify/jwt
 server.register(fastifyJwt, {
     secret: process.env.JWT_SECRET as string
+})
+
+server.decorate("authenticate", async  (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        await request.jwtVerify()
+    } catch (error) {
+        return reply.status(401).send(error)
+    }
 })
 
 const PORT = process.env.PORT || 9000;
